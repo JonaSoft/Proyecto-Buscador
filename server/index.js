160c1610fee6a4fc5ead4dbc9138c2a63aa493f4
CASA-PORTATIL\Casa <carlosucananbarrera@hotmail.com>
2018-05-05 23:48:56 -0500

    primer commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+var bodyParser = require('body-parser'),
+    http       = require('http'),
+    express    = require('express')
+    routing    = require('./requestRouting')
+
+var port       = port = process.env.PORT || 8080,
+    app        = express(),
+    Server     = http.createServer(app)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('../public'))
+app.use('/finder', routing)
+Server.listen(port, () => {
+  console.log("Servidor corriendo en el puerto: " + port)
+})
